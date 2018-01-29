3f28de2ff13b954408201a325525b3217c589ad4
Malia <Jaiden@Jaidens-Baby.local>
2018-01-28 21:13:14 -0800

    My-Commit

---
+++ repos/primitive-data-types-worksheet-jaidenmalia/Arithmetic.java
@@ -0,0 +1,74 @@
+public class Arithmetic {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		float x = 3.1f; //must put an f to indicate that it is a float
+		float y = 4.2f;
+
+		//addition
+		System.out.println();
+		System.out.println("a + a = " + (a + a));
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a + x = " + (a + x));
+		System.out.println("a + y = " + (a + y));
+		System.out.println("b + a = " + (b + a));
+		System.out.println("b + b = " + (b + b));
+		System.out.println("b + x = " + (b + x));
+		System.out.println("b + y = " + (b + y));
+		System.out.println("x + x = " + (x + x));
+		System.out.println("x + y = " + (x + y));
+		System.out.println("x + a = " + (x + a));
+		System.out.println("x + b = " + (x + b));
+		System.out.println("y + y = " + (y + y));
+		System.out.println("y + x = " + (y + x));
+		System.out.println("y + a = " + (y + a));
+		System.out.println("y + b = " + (y + b));
+
+		//division
+		System.out.println();
+		System.out.println("a / a = " + (a / a));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a / x = " + (a / x));
+		System.out.println("a / y = " + (a / y));
+		System.out.println("b / a = " + (b / a));
+		System.out.println("b / b = " + (b / b));
+		System.out.println("b / x = " + (b / x));
+		System.out.println("b / y = " + (b / y));
+		System.out.println("x / x = " + (x / x));
+		System.out.println("x / y = " + (x / y));
+		System.out.println("x / a = " + (x / a));
+		System.out.println("x / b = " + (x / b));
+		System.out.println("y / y = " + (y / y));
+		System.out.println("y / x = " + (y / x));
+		System.out.println("y / a = " + (y / a));
+		System.out.println("y / b = " + (y / b));
+
+		//subtraction
+		System.out.println();
+		System.out.println("s - s = " + (s - s));
+		System.out.println("s - l = " + (s - l));
+		System.out.println("s - d = " + (s - d));
+		System.out.println("l - l = " + (l - l));
+		System.out.println("l - s = " + (l - s));
+		System.out.println("l - d = " + (l - d));
+		System.out.println("d - d = " + (d - d));
+		System.out.println("d - l = " + (d - l));
+		System.out.println("d - s = " + (d - s));
+
+		//multiplication
+		System.out.println();
+		System.out.println("s * s = " + (s * s));
+		System.out.println("s * l = " + (s * l));
+		System.out.println("s * d = " + (s * d));
+		System.out.println("l * l = " + (l * l));
+		System.out.println("l * s = " + (l * s));
+		System.out.println("l * d = " + (l * d));
+		System.out.println("d * d = " + (d * d));
+		System.out.println("d * l = " + (d * l));
+		System.out.println("d * s = " + (d * s));
+
+
+
+
+	}
+}
