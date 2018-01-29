b4713f001459b1bf2b815128b4987483171969be
Malia <Jaiden@Jaidens-Baby.local>
2018-01-28 21:09:16 -0800

    My-Commit

---
+++ Desktop/Arithmetic.java
@@ -0,0 +1,30 @@
+public class Arithmetic {
+	public static void main(String[] args)
+	{
+		int a = 1;
+		int b = 2;
+		float x = 3.1;
+		float y = 4.2;
+		System.out.println();
+		System.out.println(a + "+" + a + "=" + (a + a));
+		System.out.println(a + "+" + b + "=" + (a + b));
+		System.out.println(a + "+" + x + "=" + (a + x));
+		System.out.println(a + "+" + y + "=" + (a + y));
+		System.out.println(b + "+" + a + "=" + (b + a));
+		System.out.println(b + "+" + b + "=" + (b + b));
+		System.out.println(b + "+" + x + "=" + (b + x));
+		System.out.println(b + "+" + y + "=" + (b + y));
+		System.out.println(x + "+" + x + "=" + (x + x));
+		System.out.println(x + "+" + y + "=" + (x + y));
+		System.out.println(x + "+" + a + "=" + (x + a));
+		System.out.println(x + "+" + b + "=" + (x + b));
+		System.out.println(y + "+" + y + "=" + (y + y));
+		System.out.println(y + "+" + x + "=" + (y + x));
+		System.out.println(y + "+" + a + "=" + (y + a));
+		System.out.println(y + "+" + b + "=" + (y + b));
+
+
+
+
+	}
+}
